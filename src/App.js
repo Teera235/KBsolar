e5b660c79c7e9f038865823b9a5d1e70bdecea38
Teera235 <yaisungnoenteerathap@gmail.com>
2026-03-03 16:08:42 +0700

    Add Vercel Analytics for visitor tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import BlogDetail from './components/BlogDetail';
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/blog/:slug" element={<BlogDetail />} />
         </Routes>
+        <Analytics />
       </div>
     </Router>
   );
